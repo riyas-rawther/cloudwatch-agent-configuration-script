6dc0e2a89dc28db343bcfb4ccf93d0f2045a3be5
Riyas Rawther <rrauther@primahealthcredit.com>
2023-06-11 12:15:53 +0530

    fix condition and variable

--- 01-modify-iis-site-logs.ps1
+++ 01-modify-iis-site-logs.ps1
@@ -24,13 +24,13 @@ foreach($site in ($iissites ))
 {
 New-Item $LogPath\$($site.Name) -type directory
 Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.directory -value "$LogPath\$($site.Name)"
-Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.truncateSize -value (truncateSize)
+Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.truncateSize -value ($truncateSize)
 Set-ItemProperty IIS:\Sites\$($site.Name) -name logfile.logTargetW3C  -Value ($logTargetW3C)
 ## (Get-ItemProperty 'IIS:\Sites\Default Web Site\' -Name logfile).logExtFileFlags
 Set-ItemProperty IIS:\Sites\$($site.Name) -Name logfile -Value @{logExtFileFlags = "Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,Referer,ProtocolVersion,Host,HttpSubStatus"}
 }
 
-if ($xforwarded) 
+if ($xforwarded -eq "true") 
 {
 ## Enabling X-Forwarded-For for Loadbalancer
 Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' `
