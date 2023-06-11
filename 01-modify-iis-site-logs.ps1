bff28d302fb253d12c291c5d07d98ea47793ed3a
Riyas Rawther <rrauther@primahealthcredit.com>
2023-06-11 12:18:27 +0530

    adding env variable import to the top

--- 01-modify-iis-site-logs.ps1
+++ 01-modify-iis-site-logs.ps1
@@ -1,14 +1,15 @@
 import-Module WebAdministration
+# reading and creating dynamic key value strings
+Foreach ($i in $(Get-Content $dir/config.env)){
+    Set-Variable -Name $i.split("=")[0] -Value $i.split("=",2)[1]
+}
 $iissites = Get-Website | Where-Object {$_.Name -ne "Default Web Site" -and  $_.State -ne "Stopped"} 
 #setting current working path to get scripts and json files
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 $LogPath = $backuppath
 $fileDateTime = $(get-date -f "MM_dd_yyyy__HH_mm_ss")
-# reading and creating dynamic key value strings
-Foreach ($i in $(Get-Content $dir/config.env)){
-    Set-Variable -Name $i.split("=")[0] -Value $i.split("=",2)[1]
-}
+
 
 #take IIS site config backup
 
