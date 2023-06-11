37b559d19ceb775bbf9f912f215755542f6c5029
Riyas Rawther <rrauther@primahealthcredit.com>
2023-06-11 09:39:58 +0530

    add X-Forwarded-For

--- 01-modify-iis-site-logs.ps1
+++ 01-modify-iis-site-logs.ps1
@@ -26,6 +26,11 @@ Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.directory -value "$LogPa
 Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.truncateSize -value "10485760"
 Set-ItemProperty IIS:\Sites\$($site.Name) -name logfile.logTargetW3C  -Value "File,ETW"
 ## (Get-ItemProperty 'IIS:\Sites\Default Web Site\' -Name logfile).logExtFileFlags
-Set-ItemProperty IIS:\Sites\$($site.Name) -Name logfile -Value @{logExtFileFlags = "Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,Win32Status,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,Cookie,Referer,ProtocolVersion,Host,HttpSubStatus"}
+Set-ItemProperty IIS:\Sites\$($site.Name) -Name logfile -Value @{logExtFileFlags = "Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,Referer,ProtocolVersion,Host,HttpSubStatus"}
 }
 
+## Enabling X-Forwarded-For for Loadbalancer
+Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' `
+     -Filter "system.applicationHost/sites/siteDefaults/logFile/customFields" `
+     -Name "." `
+     -Value @{logFieldName='X-Forwarded-For';sourceName='X-Forwarded-For';sourceType='RequestHeader'}
