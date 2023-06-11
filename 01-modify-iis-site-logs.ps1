b44789c4f3464d2fef959eab9c127e025af9fa17
Riyas Rawther <rrauther@primahealthcredit.com>
2023-06-11 12:07:08 +0530

    conditional x-forward settings and fix variable issue

--- 01-modify-iis-site-logs.ps1
+++ 01-modify-iis-site-logs.ps1
@@ -5,7 +5,6 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 $LogPath = $backuppath
 $fileDateTime = $(get-date -f "MM_dd_yyyy__HH_mm_ss")
-
 # reading and creating dynamic key value strings
 Foreach ($i in $(Get-Content $dir/config.env)){
     Set-Variable -Name $i.split("=")[0] -Value $i.split("=",2)[1]
@@ -25,14 +24,17 @@ foreach($site in ($iissites ))
 {
 New-Item $LogPath\$($site.Name) -type directory
 Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.directory -value "$LogPath\$($site.Name)"
-Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.truncateSize -value [int]$truncateSize
-Set-ItemProperty IIS:\Sites\$($site.Name) -name logfile.logTargetW3C  -Value $logTargetW3C
+Set-ItemProperty IIS:\Sites\$($site.Name) -name logFile.truncateSize -value (truncateSize)
+Set-ItemProperty IIS:\Sites\$($site.Name) -name logfile.logTargetW3C  -Value ($logTargetW3C)
 ## (Get-ItemProperty 'IIS:\Sites\Default Web Site\' -Name logfile).logExtFileFlags
 Set-ItemProperty IIS:\Sites\$($site.Name) -Name logfile -Value @{logExtFileFlags = "Date,Time,ClientIP,UserName,SiteName,ComputerName,ServerIP,Method,UriStem,UriQuery,HttpStatus,BytesSent,BytesRecv,TimeTaken,ServerPort,UserAgent,Referer,ProtocolVersion,Host,HttpSubStatus"}
 }
 
+if ($xforwarded) 
+{
 ## Enabling X-Forwarded-For for Loadbalancer
 Add-WebConfigurationProperty -PSPath 'MACHINE/WEBROOT/APPHOST' `
      -Filter "system.applicationHost/sites/siteDefaults/logFile/customFields" `
      -Name "." `
      -Value @{logFieldName='X-Forwarded-For';sourceName='X-Forwarded-For';sourceType='RequestHeader'}
+}
