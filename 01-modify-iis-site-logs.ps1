480fd372ebf118c9c5b89ad66ef90e062ddaf39f
Riyas Rawther <rrauther@primahealthcredit.com>
2023-06-14 06:15:56 +0530

    add read dir to top

--- 01-modify-iis-site-logs.ps1
+++ 01-modify-iis-site-logs.ps1
@@ -1,12 +1,13 @@
 import-Module WebAdministration
+#setting current working path to get scripts and json files
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
 # reading and creating dynamic key value strings
 Foreach ($i in $(Get-Content $dir/config.env)){
     Set-Variable -Name $i.split("=")[0] -Value $i.split("=",2)[1]
 }
 $iissites = Get-Website | Where-Object {$_.Name -ne "Default Web Site" -and  $_.State -ne "Stopped"} 
-#setting current working path to get scripts and json files
-$scriptpath = $MyInvocation.MyCommand.Path
-$dir = Split-Path $scriptpath
+
 $LogPath = $backuppath
 $fileDateTime = $(get-date -f "MM_dd_yyyy__HH_mm_ss")
 
